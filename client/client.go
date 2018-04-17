fea28116604a4ba80a855642a4e8f5c87c7e211e
Tony Yip <tony@opensource.hk>
2018-04-17 23:53:30 +0800

    Add get client from env

---
+++ client/client.go
@@ -0,0 +1,22 @@
+package client
+
+import (
+	"os"
+
+	"github.com/minio/minio-go"
+)
+
+// NewFromEnv create minio client from environment variables
+func NewFromEnv() (*minio.Client, error) {
+	endpoint := os.Getenv("S3_ENDPOINT")
+	if endpoint == "" {
+		endpoint = "s3.amazonaws.com"
+	}
+
+	return minio.New(
+		endpoint,
+		os.Getenv("S3_ACCESS_KEY_ID"),
+		os.Getenv("S3_SECRET_ACCESS_KEY"),
+		os.Getenv("S3_INSECURE") == "",
+	)
+}
