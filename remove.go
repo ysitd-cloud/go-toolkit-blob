80f719382800f5f26dee12aa430239b8da184038
Tony Yip <tony@opensource.hk>
2018-04-18 00:40:16 +0800

    Add remove with prefix

---
+++ remove.go
@@ -0,0 +1,28 @@
+package blob
+
+import (
+	"github.com/minio/minio-go"
+)
+
+func RemovePrefix(client *minio.Client, bucketName, objectPrefix string, recursive bool) []error {
+	objectsCh := make(chan string)
+
+	errs := make([]error, 0)
+
+	// Send object names that are needed to be removed to objectsCh
+	go func() {
+		defer close(objectsCh)
+		// List all objects from a bucket-name with a matching prefix.
+		for object := range client.ListObjects("my-bucketname", "my-prefixname", true, nil) {
+			if object.Err != nil {
+				errs = append(errs, object.Err)
+			} else {
+				objectsCh <- object.Key
+			}
+		}
+	}()
+	for rErr := range client.RemoveObjects("mybucket", objectsCh) {
+		errs = append(errs, rErr.Err)
+	}
+	return errs
+}
